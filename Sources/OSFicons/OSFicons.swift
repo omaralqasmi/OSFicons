33143cfb6e6ba6a8ab37f228c501e57e318f37a7
Omar AlQasmi <omar.qasmi@innovateq.digital>
2023-09-03 13:53:57 +0400

    in module

--- Sources/OSFicons/OSFicons.swift
+++ Sources/OSFicons/OSFicons.swift
@@ -5,9 +5,9 @@ public struct OSFicons {
     public init() {
     }
     public static func image(named: OSFiconsNamesHierarchical) -> UIImage{
-        return UIImage(named: named.rawValue) ?? UIImage()
+        return UIImage.init(named: named.rawValue, in: .module, compatibleWith: nil)!
     }
     public static func image(named: OSFiconsNamesMono) -> UIImage{
-        return UIImage(named: named.rawValue) ?? UIImage()
+        return UIImage.init(named: named.rawValue, in: .module, compatibleWith: nil)!
     }
 }
